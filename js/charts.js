3736ecf87b444ef3903250f168e35ca37af4207f
Gizem <gizem@Gizem.gzfs.dev>
2024-01-22 21:19:08 +0530

    chore: initial

---
+++ js/charts.js
@@ -0,0 +1,120 @@
+function index_to_shortname( index ){
+    return ["common_directories","x_frame_options","server_header","insecure_cors_policy","interesting_responses","emails"][index];
+}
+
+function index_to_severity( index ){
+    return {"common_directories":"medium","x_frame_options":"low","server_header":"low","insecure_cors_policy":"low","interesting_responses":"informational","emails":"informational"}[index_to_shortname(index)];
+}
+
+function renderCharts() {
+    if( window.renderedCharts )
+    window.renderedCharts = true;
+
+    c3.generate({
+        bindto: '#chart-issues',
+        data: {
+            columns: [
+                ["Trusted",3,1,1,1,7,1],
+                ["Untrusted",0,0,0,0,0,0],
+                ["Severity",3,2,2,2,1,1]
+            ],
+            axes: {
+                Severity: 'y2'
+            },
+            type: 'bar',
+            groups: [
+                ['Trusted', 'Untrusted']
+            ],
+            types: {
+                Severity: 'line'
+            },
+            onclick: function (d) {
+                var location;
+
+                if( d.name.toLowerCase() == 'severity' ) {
+                    location = 'summary/issues/trusted/severity/' + index_to_severity(d.x);
+                } else {
+                    location = 'summary/issues/' + d.name.toLowerCase() + '/severity/' +
+                        index_to_severity(d.x) + '/' + index_to_shortname(d.x);
+                }
+
+                goToLocation( location );
+            }
+        },
+        regions: [{"class":"severity-medium","start":0,"end":0},{"class":"severity-low","start":1,"end":3},{"class":"severity-informational","start":4}],
+        axis: {
+            x: {
+                type: 'category',
+                categories: ["Common directory","Missing 'X-Frame-Options' header","'Server' header","Insecure 'Access-Control-Allow-Origin' header","Interesting response","E-mail address disclosure"],
+                tick: {
+                    rotate: 15
+                }
+            },
+            y: {
+                label: {
+                    text: 'Amount of logged issues',
+                    position: 'outer-center'
+                }
+            },
+            y2: {
+                label: {
+                    text: 'Severity',
+                    position: 'outer-center'
+                },
+                show: true,
+                type: 'category',
+                categories: [1, 2, 3, 4],
+                tick: {
+                    format: function (d) {
+                        return ["Informational","Low","Medium","High"][d - 1]
+                    }
+                }
+            }
+        },
+        padding: {
+            bottom: 40
+        },
+        color: {
+            pattern: [ '#1f77b4', '#d62728', '#ff7f0e' ]
+        }
+    });
+
+    c3.generate({
+        bindto: '#chart-trust',
+        data: {
+            type: 'pie',
+            columns: [["Trusted",14],["Untrusted",0]]
+        },
+        pie: {
+            onclick: function (d) { goToLocation( 'summary/issues/' + d.id.toLowerCase() ) }
+        },
+        color: {
+            pattern: [ '#1f77b4', '#d62728' ]
+        }
+    });
+
+    c3.generate({
+        bindto: '#chart-elements',
+        data: {
+            type: 'pie',
+            columns: [["body",1],["server",13]]
+        }
+    });
+
+    c3.generate({
+        bindto: '#chart-severities',
+        data: {
+            type: 'pie',
+            columns: [["medium",3],["low",3],["informational",8]]
+        },
+        color: {
+            pattern: [ '#d62728', '#ff7f0e', '#ffbb78', '#1f77b4' ]
+        },
+        pie: {
+            onclick: function (d) {
+                goToLocation( 'summary/issues/trusted/severity/' + d.id );
+            }
+        }
+    });
+
+}
